23e93fcac7199107552f79d1d2471218caad07ca
Fırat Sivrikaya <sivrikayafirat@gmail.com>
2018-05-16 12:16:27 +0300

    PanelChangeListener implemented

---
+++ src/controller/PanelChangeListener.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class PanelChangeListener implements ActionListener {
+	
+	private JPanel panelFrom;
+	private JPanel panelTo;
+	
+	
+	public PanelChangeListener(JPanel login, JPanel home) {
+		super();
+		this.panelFrom = login;
+		this.panelTo = home;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		panelFrom.setVisible(false);
+		panelTo.setVisible(true);
+	}
+
+}
